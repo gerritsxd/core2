bfee356f516b2842f0e0e8cc92632a992ad06a5f
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2025-06-07 09:22:56 +0200

    updated deployment script

--- _deploy.sh
+++ _deploy.sh
@@ -6,37 +6,55 @@
 ### To edit the cron file with nano instead of vim:
 ### export VISUAL=nano; crontab -e
 
-echo ----------
-echo $(date)
+echo - - - STARTING DEPLOYMENT SCRIPT at $(date) - - -
 
-PATH=$PATH:/opt/plesk/phpenv/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+echo Setting variables:
+
+quartoDir = "/usr/local/bin/quarto"
+extraPathDirs = "/opt/plesk/phpenv/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin"
+httpDir = "~/httpdocs"
+renderDir = "public"
+repoDir = "core"
+deployDir = "~/deploy_core"
+
+echo quartoDir = $quartoDir
+echo extraPathDirs = $extraPathDirs
+echo httpDir = $httpDir
+echo renderDir = $renderDir
+echo repoDir = $repoDir
+echo deployDir = $deployDir
+
+PATH=$PATH:$extraPathDirs
+
+echo Updated $PATH to also include $extraPathDirs
 
 ### Go to directory with cloned git repo
-cd ~/deploy_core
+cd $deployDir
 
-echo Running Quarto...
+echo Running Quarto in deployment directory $deployDir...
 
 ### Render the site
-if /usr/local/bin/quarto render; then
+if $quartoDir render; then
 
 # https://unix.stackexchange.com/questions/22726/how-to-conditionally-do-something-if-a-command-succeeded-or-failed
 
-  echo Done with Quarto. Deleting old directories and files...
+  echo Done with Quarto. Deleting old directories and files from $httpDir...
   
   ### Delete all contents in public HTML directory
-  rm -rf ~/core/*.*
-  rm -rf ~/core/*
-  #rm -f ~/core/.htaccess
-  rm -f ~/core/core/.htaccess
+  rm -rf $httpDir/*.*
+  rm -rf $httpDir/*
+  #rm -f $httpDir/.htaccess
+  rm -f $httpDir/$repoDir/.htaccess
   
-  echo Done deleting old directories and files. Copying over new website...
+  echo Done deleting old directories and files. Copying over new website from $deployDir/$renderDir to $httpDir...
   
-  ### Copy website
-  cp -RT public ~/core
+  ### Copy website: "httpdocs" should be changed to
+  ### the directory the website is served from
+  cp -RT $renderDir $httpDir
   
   ### Copy .htaccess
   #cp .htaccess ~/comms.opens.science/core
-  cp core/.htaccess ~/core/core
+  cp $repoDir/.htaccess $httpDir/$repoDir
 
   echo Done copying over new website.
 
@@ -46,4 +64,4 @@ else
 
 fi
 
-echo ----------
+echo - - - ENDING DEPLOYMENT SCRIPT at $(date) - - -
